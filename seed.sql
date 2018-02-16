fb9f04d9536125e0a6a75b5dfe1c8a6cf497844e
Elizabeth <erischjanson@gmail.com>
2018-02-16 17:49:35 -0600

    first commit

---
+++ seed.sql
@@ -0,0 +1,44 @@
+USE bamazon;
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES("Chocolate", "Grocery", 2.25, 95);
+
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES();
+
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES();
+
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES();
+
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES();
+
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES();
+
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES();
+
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES();
+
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES();
+
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES();
+
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES();
