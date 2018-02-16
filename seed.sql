4b4c97344a0b71fc5f3e911b8c99ff8f94117e00
Elizabeth <erischjanson@gmail.com>
2018-02-16 17:56:36 -0600

    product table populated

--- seed.sql
+++ seed.sql
@@ -5,40 +5,40 @@ VALUES("Chocolate", "Grocery", 2.25, 95);
 
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES();
+VALUES("Blood on the Tracks", "Music", 13.99, 50);
 
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES();
+VALUES("Ivory Soap", "Health and Beauty", 3.50, 100);
 
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES();
+VALUES("Toothbrush", "Health and Beauty", 1.99, 67);
 
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES();
+VALUES("Tylenol PM", "Pharmacy", 5.95, 88);
 
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES();
+VALUES("Jeans", "Clothing", 55.00, 15);
 
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES();
+VALUES("Notebook", "Office Supplies", 2.50, 25);
 
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES();
+VALUES("Freedom", "Books", 13.99, 60);
 
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES();
+VALUES("Crime and Punishment", "Books", 9.99, 5);
 
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES();
+VALUES("Milk", "Grocery", 2.95, 99);
 
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES();
+VALUES("Sour Patch Kids", "Grocery", 1.99, 55);
