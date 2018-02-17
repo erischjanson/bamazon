74fbda9ad98079289f47dea3d71171f6e2159609
Elizabeth <erischjanson@gmail.com>
2018-02-17 15:02:16 -0600

    added chooseItem function to retrieve data about user's chosen item

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,25 +18,56 @@ connection.connect(function(error){
 
 	console.log("You are connected as ID " + connection.threadId);
 	queryAll();
+	
 });
 
+//returns all products along with their price and id
 function queryAll(){
-	connection.query("SELECT * FROM products", function(err, answer){
-		if(err){
-			console.log(err);
+	connection.query("SELECT * FROM products", function(error, response){
+		if(error){
+			console.log(error);
 		}
-		console.log(answer);		
-		for (var i = 0; i < answer.length; i ++){
-			console.log("ITEM: ", answer[i].product_name);
-			console.log("PRICE: ", answer[i].price);
-			console.log("ID: ", answer[i].item_id);
+		
+		console.log("ITEMS FOR SALE ON BAMAZON:" + "\n");		
+		for (var i = 0; i < response.length; i ++){
+			console.log("ITEM: ", response[i].product_name);
+			console.log("PRICE: ", response[i].price);
+			console.log("ID: ", response[i].item_id);
 			console.log("\n");
 			console.log("----------------------------------------")
 			console.log("\n");
 		}
+
+		chooseItem();
 	});
 
 };
+
+//inquirer prompts to find out what the user wants
+function chooseItem(){
+
+	inquirer.prompt([
+{
+	type:"input",
+	message: "Enter the ID of the item you wish to purchase.",
+	name: "idToBuy"
+},
+
+{
+	type: "input",
+	message: "How many units would you like to buy?",
+	name: "units"
+}
+	]).then(function(answer){
+		console.log(answer);
+		var query = "SELECT * FROM products WHERE ?";
+		connection.query(query, {item_id: answer.idToBuy}, function(error, results){
+			console.log(results);
+		});
+	});
+
+};
+
 
 
 
